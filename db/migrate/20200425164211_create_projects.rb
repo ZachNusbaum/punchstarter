815e1a2386f94d6c7bd6d5e276bde37470bfb553
Zach Nusbaum <zach@zdn.dev>
2020-04-25 09:45:33 -0700

    Generate product scaffold

---
+++ db/migrate/20200425164211_create_projects.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.decimal :donation_goal, default: 0
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

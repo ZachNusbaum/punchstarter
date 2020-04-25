555ffb260ff2e01adea2d72f311307dd58021177
Zach Nusbaum <zach@zdn.dev>
2020-04-25 10:28:59 -0700

    style projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def author_of(_resource)
+    user_signed_in? && _resource.user_id == current_user.id
+  end
+
+  def admin?
+    user_signed_in? && current_user.admin?
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class Project < ApplicationRecord
+  has_rich_text :description
   belongs_to :user
 end
